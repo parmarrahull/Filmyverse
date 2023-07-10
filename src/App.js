0ae78de1a693f4658eddf0745fe9c05daf5c5cb4
unknown <parmarrahul1974@gmail.com>
2023-07-10 09:24:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { createContext, useState } from 'react'
+import Header from "./components/Header"
+import Cards from './components/Cards'
+import AddMovie from "./components/AddMovie"
+import Detail from "./components/Detail"
+import { Route, Routes } from 'react-router-dom'
+// import Login from "./components/Login"
+// import Signup from "./components/Signup"
+
+const Appstate = createContext();
+
+const App = () => {
+  const [login, setLogin] = useState(false);
+  const [userName, setUserName] = useState("");
+
+  return (
+    <>
+      <Appstate.Provider value={{ login, userName, setLogin, setUserName }} >
+        <div className="App relative">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Cards />} />
+            <Route path="/addmovie" element={<AddMovie />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            {/* <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} /> */}
+          </Routes>
+        </div>
+      </Appstate.Provider>
+    </>
+  )
+}
+
+export default App
+export {Appstate}
---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyClc9OTJlL26MB9Kf2damGtfjDAVP_G9Gg",
+    authDomain: "filmyverse-479cd.firebaseapp.com",
+    projectId: "filmyverse-479cd",
+    storageBucket: "filmyverse-479cd.appspot.com",
+    messagingSenderId: "584720320211",
+    appId: "1:584720320211:web:dcae2c177c3f24982fb626"
+};
+
+const apps = initializeApp(firebaseConfig);
+
+
+export const db = getFirestore(apps);
+export const moviesRef = collection(db, "movies");
+export const reviewsRef = collection(db, "reviews");
+// export const usersRef = collection(db, "users");
+
+export default apps
